ce166330d7a07f8eec2b402c30c77f806dd6b9e9
VShivam0808 <vshivamgithub003@gmail.com>
2025-03-14 00:35:04 +0530

    Initial commit to new repository

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="font-bold bg-cyan-400 w-50 h-10 text-gray-700" >
+          Hello world !
         </p>
         <a
           className="App-link"
